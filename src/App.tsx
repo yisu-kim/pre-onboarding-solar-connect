e6edfd62e9ceecd2837f04d0941b219571289bca
yisu-kim <yisu.kim.dev@outlook.com>
2021-08-19 19:17:12 +0900

    Chore: Fetch assignment codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "antd/dist/antd.css";
+import Spinner from "./components/common/Spinner";
+import TodoContainer from "./components/todo/TodoContainer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  //@TODO login
+  let isLogged = true;
+
+  const RenderLayout = (
+    <div>
+      <TodoContainer />
     </div>
   );
+
+  return isLogged ? RenderLayout : <Spinner mask />;
 }
 
 export default App;
